d08796274f8c5f239a6dc73e6a8f158d174638c1
Stephen Kunath <skunath@gmail.com>
2017-02-18 12:09:09 -0500

    changed db location

--- basic_flask.py
+++ basic_flask.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///firstTest.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/production.db'
 Bootstrap(app)
 db = SQLAlchemy(app)
 
